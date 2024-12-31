830f84ae7a12abcd8c292373c77bbdeb86dcb68b
Anastasia Baburova <nas3235@yandex.ru>
2024-12-31 18:34:07 +0300

    Задание 1 полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,3 +1,27 @@
+"use strict";
+
+function solveEquation(a, b, c) {
+  // Вычисляем дискриминант
+  const discriminant = b ** 2 - 4 * a * c;
+
+  // Если дискриминант меньше нуля, корней нет
+  if (discriminant < 0) {
+    return [];
+  }
+
+  // Если дискриминант равен нулю, один корень
+  if (discriminant === 0) {
+    const root = -b / (2 * a);
+    return [root];
+  }
+
+  // Если дискриминант больше нуля, два корня
+  const sqrtDiscriminant = Math.sqrt(discriminant);
+  const root1 = (-b + sqrtDiscriminant) / (2 * a);
+  const root2 = (-b - sqrtDiscriminant) / (2 * a);
+  return [root1, root2];
+}
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   // Преобразуем процентную ставку из диапазона 0-100 в 0-1 и вычисляем месячную ставку
   const monthlyRate = percent / 100 / 12;
