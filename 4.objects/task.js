bdd67dee2b8c18757a10e1c5372cb90dc4de0f5f
Anastasia Baburova <nas3235@yandex.ru>
2025-01-21 20:47:14 +0300

    Задание 4 полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,5 +1,3 @@
-//Задание 1
-
 function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
@@ -21,18 +19,16 @@ Student.prototype.getAverage = function () {
   if (!this.marks || this.marks.length === 0) {
     return 0;
   }
-
-  const total = this.marks.reduce((acc, mark) => acc + mark, 0);
-  return total / this.marks.length;
+  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+  return sum / this.marks.length;
 };
 
 Student.prototype.exclude = function (reason) {
   delete this.subject;
   delete this.marks;
-  this.excludeReason = reason;
+  this.excluded = reason;
 };
 
-// Пример использования функции-конструктора
 let student1 = new Student("Василиса", "женский", 19);
 student1.setSubject("Algebra");
 console.log(student1.getAverage()); // 0
@@ -40,8 +36,9 @@ student1.addMarks(4, 5, 4, 5);
 console.log(student1.getAverage()); // 4.5
 console.log(student1);
 // {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+
 let student2 = new Student("Артём", "мужской", 25);
 student2.setSubject("Geometry");
-student2.exclude("прогулы");
+student2.exclude("плохая учёба");
 console.log(student2);
-// {name: "Артём", gender: "мужской", age: 25, excluded: "прогулы"}
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
