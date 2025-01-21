66154a374fceefbc2c715ed2119c4a84afde14ea
Anastasia Baburova <nas3235@yandex.ru>
2025-01-21 21:58:45 +0300

    Задание 5 полностью готово

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -95,3 +95,154 @@ picknick.state = 10;
 console.log(picknick.state); // 10
 picknick.fix();
 console.log(picknick.state); // 15
+
+//Задача 2
+
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    return this.books.find((book) => book[type] === value) || 0;
+  }
+
+  giveBookByName(bookName) {
+    const bookIndex = this.books.findIndex((book) => book.name === bookName);
+    if (bookIndex !== -1) {
+      return this.books.splice(bookIndex, 1)[0];
+    }
+    return 0;
+  }
+}
+
+class DetectiveBook {
+  constructor(author, name, releaseDate, state) {
+    this.author = author;
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.state = state;
+  }
+}
+
+class FantasticBook {
+  constructor(author, name, releaseDate, state) {
+    this.author = author;
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.state = state;
+  }
+}
+
+class NovelBook {
+  constructor(author, name, releaseDate, state) {
+    this.author = author;
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.state = state;
+  }
+}
+
+class Magazine {
+  constructor(name, releaseDate, state) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.state = state;
+  }
+}
+
+// Пример использования:
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(
+  new DetectiveBook(
+    "Артур Конан Дойл",
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+  )
+);
+library.addBook(
+  new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+  )
+);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.findBookBy("name", "Властелин колец")); // null
+console.log(library.findBookBy("releaseDate", 1924).name); // "Мурзилка"
+
+console.log("Количество книг до выдачи: " + library.books.length); // Количество книг до выдачи: 4
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); // Количество книг после выдачи: 3
+
+// Создание библиотеки
+const myLibrary = new Library("Моя библиотека");
+
+// Добавление книг разных типов
+const book1 = new DetectiveBook(
+  "Артур Конан Дойл",
+  "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+  2019,
+  1008
+);
+const book2 = new FantasticBook(
+  "Аркадий и Борис Стругацкие",
+  "Пикник на обочине",
+  1972,
+  168
+);
+const book3 = new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138);
+const book4 = new Magazine("Мурзилка", 1924, 60);
+
+myLibrary.addBook(book1);
+myLibrary.addBook(book2);
+myLibrary.addBook(book3);
+myLibrary.addBook(book4);
+
+// Попытка найти книгу, изданную в 1919 году
+let book1919 = myLibrary.findBookBy("releaseDate", 1919);
+if (!book1919) {
+  // Если книга не найдена, создать её
+  book1919 = new DetectiveBook("Unknown", "Mystery of 1919", 1919, 60);
+  myLibrary.addBook(book1919);
+}
+
+// Выдача книги
+const issuedBook = myLibrary.giveBookByName("Пикник на обочине");
+console.log("Выдана книга:", issuedBook.name);
+
+// Повреждение выданной книги
+issuedBook.state = 20; // Например, состояние 20 из 100
+console.log("Книга повреждена. Новое состояние:", issuedBook.state);
+
+// Восстановление книги
+issuedBook.state = 50; // Восстанавливаем состояние до 50
+console.log("Книга восстановлена. Новое состояние:", issuedBook.state);
+
+// Попытка добавить восстановленную книгу обратно в библиотеку
+myLibrary.addBook(issuedBook); // Полезно проверить, добавится ли книга
+if (myLibrary.books.includes(issuedBook)) {
+  console.log("Книга успешно добавлена обратно в библиотеку.");
+} else {
+  console.log("Книгу не удалось добавить обратно в библиотеку.");
+}
+
+// Проверка текущего состояния библиотеки
+console.log("Текущие книги в библиотеке:");
+myLibrary.books.forEach((book) => {
+  console.log(
+    `Название: ${book.name}, Автор: ${book.author}, Год: ${book.releaseDate}, Состояние: ${book.state}`
+  );
+});
